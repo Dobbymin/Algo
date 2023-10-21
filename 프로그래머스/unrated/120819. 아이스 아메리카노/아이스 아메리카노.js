7e3ad839604abdf6c3a19432e38f941aadff9df0
김강민 <79429482+Dobbymin@users.noreply.github.com>
2023-10-21 21:41:29 +0900

    [level unrated] Title: 아이스 아메리카노, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120819. 아이스 아메리카노/아이스 아메리카노.js
@@ -0,0 +1,7 @@
+function solution(money) {
+        const answer = [];
+        const americano = 5500;
+        answer[0] = Math.floor(money / americano);
+        answer[1] = money % americano;
+    return answer;
+}
