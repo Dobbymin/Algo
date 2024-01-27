c59ffbcb888c1063156a5cf26fed03d2bcdc8fc7
김강민 <79429482+Dobbymin@users.noreply.github.com>
2024-01-27 13:53:43 +0900

    [level 0] Title: 홀짝 구분하기, Time: 18.04 ms, Memory: 7.3 MB -BaekjoonHub

--- 프로그래머스/0/181944. 홀짝 구분하기/홀짝 구분하기.py
+++ 프로그래머스/0/181944. 홀짝 구분하기/홀짝 구분하기.py
@@ -1,2 +1,5 @@
 a = int(input())
-print(f'{a} is even') if a % 2 == 0 else print(f'{a} is odd')
+# print(f'{a} is even') if a % 2 == 0 else print(f'{a} is odd')
+
+print(f"{a} is {'even' if a % 2 == 0 else 'odd'}")
+
