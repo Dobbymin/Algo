4df920a9eace1d3baca547980711b3ea1ad1cdbf
김강민 <79429482+Dobbymin@users.noreply.github.com>
2024-01-28 14:05:47 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.py
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.py
@@ -1,4 +1,2 @@
 def solution(my_string, overwrite_string, s):
-    str_length = len(my_string[0:s] + overwrite_string)
-    answer = my_string[0:s] + overwrite_string + my_string[str_length:]
-    return answer
+    return my_string[:s] + overwrite_string + my_string[s + len(overwrite_string):]
