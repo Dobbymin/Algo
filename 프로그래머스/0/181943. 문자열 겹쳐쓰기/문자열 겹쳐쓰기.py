102fa74a4ce6de5f091b3bef87b803ad595a53f1
김강민 <79429482+Dobbymin@users.noreply.github.com>
2024-01-28 14:03:29 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.00 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.py
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.py
@@ -1,6 +1,4 @@
 def solution(my_string, overwrite_string, s):
-    # answer = my_string[0:s] + overwrite_string + my_string[-s+1:]
-    # answer = my_string[-s+1:]
     str_length = len(my_string[0:s] + overwrite_string)
     answer = my_string[0:s] + overwrite_string + my_string[str_length:]
     return answer
