2b1da5d50f04bc739cb5cf9a672ebeb24c501793
김강민 <79429482+Dobbymin@users.noreply.github.com>
2024-02-09 01:08:51 +0900

    [level 0] Title: ad 제거하기, Time: 0.06 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/0/181870. ad 제거하기/ad 제거하기.py
+++ 프로그래머스/0/181870. ad 제거하기/ad 제거하기.py
@@ -1,9 +1,6 @@
 def solution(strArr):
-    for i in range(len(strArr)):
-        for k in strArr:
-            if 'ad' in k:
-                strArr.remove(k)
-        # if 'ad' in strArr[i]:
-        #     strArr.remove(strArr[i])
-    
-    return strArr
+    answer = []
+    for k in strArr:
+        if "ad" not in k:
+            answer.append(k)
+    return answer
