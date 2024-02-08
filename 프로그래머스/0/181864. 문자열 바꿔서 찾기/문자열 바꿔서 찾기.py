fc39d3b0e5450895cd42b65b821812315442a443
김강민 <79429482+Dobbymin@users.noreply.github.com>
2024-02-08 11:31:02 +0900

    [level 0] Title: 문자열 바꿔서 찾기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.py
+++ 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.py
@@ -1,14 +1,5 @@
 def solution(myString, pat):
-    answer = 0
-    rvs_myString = ''
     
-    for k in myString:
-        if k == 'A':
-            rvs_myString += 'B'
-        elif k == 'B':
-            rvs_myString += 'A'
+    Pat = pat.replace('A', 'C').replace('B', 'A').replace('C', 'B')
     
-    if pat in rvs_myString:
-        return 1
-    else:
-        return 0
+    return int(Pat in myString)
