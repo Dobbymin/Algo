1ce83bd3acd87c0c2ca720c0f996c32c8aacbace
김강민 <79429482+Dobbymin@users.noreply.github.com>
2024-02-10 19:26:50 +0900

    [level 0] Title: 날짜 비교하기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.py
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.py
@@ -1,6 +1,2 @@
-from datetime import datetime
 def solution(date1, date2):
-    day1 = datetime(date1[0], date1[1], date1[2])
-    day2 = datetime(date2[0], date2[1], date2[2])
-    
-    return 1 if day1 < day2 else 0
+    return int(date1 < date2)
