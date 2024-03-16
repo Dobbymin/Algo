a1cfaf64b3421194acc895c9986d7e8de5c57034
김강민 <79429482+Dobbymin@users.noreply.github.com>
2024-03-16 22:58:20 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.py
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.py
@@ -1,7 +1,4 @@
 def solution(n):
-    answer = ''
-    pattern = '수박'
-    if n % 2 == 0:
-        return pattern * (n // 2)
-    elif n % 2 != 0:
-        return pattern * (n // 2) + pattern[:1]
+    pattern = '수박' * n
+    return pattern[:n]
+    
