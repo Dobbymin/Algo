9965dd10f1a004efb4294787ec0a6290cf1ec316
김강민 <79429482+Dobbymin@users.noreply.github.com>
2024-08-09 14:21:52 +0900

    [level 0] Title: 정수 찾기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181840. 정수 찾기/정수 찾기.js
+++ 프로그래머스/0/181840. 정수 찾기/정수 찾기.js
@@ -1,7 +1,1 @@
-function solution(num_list, n) {
-    var answer = 0;
-    for (let i = 0; i < num_list.length; i++) {
-        if (num_list[i] === n) answer = 1;
-    }
-    return answer;
-}
+const solution = (num_list, n) => num_list.includes(n) ? 1 : 0;
