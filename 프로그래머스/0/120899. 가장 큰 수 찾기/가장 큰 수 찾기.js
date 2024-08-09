4875feba80619226cbf33458eeea8a4a9fb97b62
김강민 <79429482+Dobbymin@users.noreply.github.com>
2024-08-09 22:58:52 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.js
+++ 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.js
@@ -1,14 +1,3 @@
 function solution(array) {
-    var answer = [];
-    let max = 0;
-    let idx = 0;
-    for (let i = 0; i < array.length; i++) {
-        if (array[i] > max) {
-            max = array[i]
-            idx = i;
-        }
-    }
-    answer.push(max);
-    answer.push(idx);
-    return answer;
+    return [Math.max(...array), array.indexOf(Math.max(...array))];
 }
