fccfb70c87f48bf52edc47960a1d65c60f7846ed
김강민 <79429482+Dobbymin@users.noreply.github.com>
2025-05-01 16:13:52 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
@@ -1,13 +1,5 @@
 function solution(seoul) {
-    let answer = '';
-    let position = 0;
+    let index = seoul.indexOf('Kim');
     
-    for (let i = 0; i < seoul.length; i++) {
-        if (seoul[i] === 'Kim') {
-            position = i;
-        }
-    }
-    
-    answer = `김서방은 ${position}에 있다`;
-    return answer;
+    return `김서방은 ${index}에 있다`;
 }
