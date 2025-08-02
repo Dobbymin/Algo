dcbb446b535c42301b33001b856f072a1851716d
김강민 <79429482+Dobbymin@users.noreply.github.com>
2025-08-02 13:17:55 +0900

    [level 0] Title: 문자열 돌리기, Time: 44.43 ms, Memory: 32.2 MB -BaekjoonHub

--- 프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.js
+++ 프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.js
@@ -10,8 +10,7 @@ rl.on('line', function (line) {
     input = [line];
 }).on('close',function(){
     str = input[0];
-    
-    for(let i = 0; i < str.length; i++) {
+    for (let i = 0; i < str.length; i++) {
         console.log(str[i]);
     }
 });
