23794806583eae2792c079765813f07d93cb813b
김강민 <79429482+Dobbymin@users.noreply.github.com>
2025-08-05 08:02:56 +0900

    [level 0] Title: 두 수의 연산값 비교하기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.js
+++ 프로그래머스/0/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.js
@@ -1,18 +1,4 @@
-const mark = (a, b) => {
-    return Number(String(a) + String(b));
-}
-
-const multiple = (a, b) => {
-    return 2 * a * b;
-}
-
-const solution = (a, b) => {
-    let markNumber = mark(a, b);
-    let multipleNumber = multiple(a, b);
-    
-    if (markNumber === multipleNumber) {
-        return markNumber;
-    } else {
-        return Math.max(markNumber, multipleNumber);
-    }
+function solution(a, b) {
+    var answer = 0;
+    return Math.max((`${a}${b}`), (2*a*b));
 }
