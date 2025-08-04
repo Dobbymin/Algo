fe4f3d4a228396755a6caf808c23866967f189d3
김강민 <79429482+Dobbymin@users.noreply.github.com>
2025-08-04 20:11:21 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.js
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.js
@@ -1,5 +1,5 @@
 function solution(my_string, overwrite_string, s) {
-    var answer = [...my_string];
-    answer.splice(s, overwrite_string.length, overwrite_string);
-    return answer.join('');
+    let string_arr = [...my_string];
+    string_arr.splice(s, overwrite_string.length, overwrite_string);
+    return string_arr.join('');
 }
