2186d7570c589ea5e424388ca76471188cc603c7
김강민 <79429482+Dobbymin@users.noreply.github.com>
2025-10-23 20:51:16 +0900

    [level 0] Title: 최빈값 구하기, Time: 4.69 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.js
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.js
@@ -1,18 +1,20 @@
 function solution(array) {
     const set = new Set(array);
     const arr = Array.from(set);
+    
+    console.log(arr);
+    
     const result = arr.map((i) => {
-        return array.filter((element) => element === i).length
-    });
+        return array.filter((element) => i === element).length;
+    })
     
     const max = Math.max(...result);
     
-    const num = result.filter((element) => max === element).length
+    const num = result.filter((element) => max === element).length;
     
     if (num > 1) {
         return -1;
-    }
-    else {
+    } else {
         const index = result.findIndex((e) => e === max);
         return arr[index];
     }
