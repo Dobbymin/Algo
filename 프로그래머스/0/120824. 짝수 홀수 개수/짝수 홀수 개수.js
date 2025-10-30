51994e52b48af2cf1dbfff461b7ae4b784129c3f
김강민 <79429482+Dobbymin@users.noreply.github.com>
2025-10-30 23:14:04 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
@@ -1,6 +1,5 @@
 function solution(num_list) {
     const even = num_list.filter((num) => num % 2 === 0);
-    const odd = num_list.filter((num) => num % 2 !== 0);
 
-    return [even.length, odd.length];
+    return [even.length, num_list.length - even.length];
 }
