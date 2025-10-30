363cc31d703f2faffe5bc8e9ff6158f4b31e7d85
김강민 <79429482+Dobbymin@users.noreply.github.com>
2025-10-30 23:15:58 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.js
+++ 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.js
@@ -1,5 +1,9 @@
 function solution(array, height) {
-    var answer = array.filter(item => item > height);
-    return answer.length;
+    var answer = 0;
+    array.forEach((h) => {
+        if (h > height) {
+            answer++;
+        }
+    })
+    return answer;
 }
-
