ea32031303d421e2cfb9041e0218f9b78dd3e3a5
김강민 <79429482+Dobbymin@users.noreply.github.com>
2025-10-31 21:33:48 +0900

    [level 0] Title: 모음 제거, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.js
@@ -0,0 +1,11 @@
+function solution(my_string) {
+    var answer = '';
+    const arr = ['a', 'e', 'i', 'o', 'u'];
+    
+    for (let i = 0; i < my_string.length; i++) {
+        if(!arr.includes(my_string[i])) {
+            answer += my_string[i];
+        }
+    }
+    return answer;
+}
