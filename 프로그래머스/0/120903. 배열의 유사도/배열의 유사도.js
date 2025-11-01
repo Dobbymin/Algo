42bc79ebe5200849b0467f6fbc582efe4db01637
김강민 <79429482+Dobbymin@users.noreply.github.com>
2025-11-01 11:12:16 +0900

    [level 0] Title: 배열의 유사도, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.js
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.js
@@ -1,17 +1,9 @@
 function solution(s1, s2) {
-    let answer = 0;
-    if (s1.length > s2.length) {
-        for (let i = 0; i < s2.length; i++) {
-            if (s1.includes(s2[i])) {
-                answer += 1;
-            }
+    var answer = 0;
+    s2.forEach((str) => {
+        if(s1.includes(str)) {
+            answer += 1;
         }
-    } else {
-        for (let i = 0; i < s1.length; i++) {
-            if (s2.includes(s1[i])) {
-                answer += 1;
-            }
-        }
-    }
+    })
     return answer;
 }
