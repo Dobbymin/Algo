812620616302f70081a1c65d61e1251dc8738acb
김강민 <79429482+Dobbymin@users.noreply.github.com>
2026-02-04 23:32:29 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 39.27 ms, Memory: 82.8 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -2,15 +2,16 @@ function solution(arr)
 {
     var answer = [];
 
-    for (let i = 0; i < arr.length; i++) {
-        if (i === 0) {
-            answer.push(arr[i]);
-        }
+    for(let i = 0; i < arr.length; i++) {
+        if (i === 0) answer.push(arr[i]);
         
-        else if (i !== 0 && arr[i-1] !== arr[i]) {
-            answer.push(arr[i]);
+        if (i > 0) {
+            if (arr[i-1] !== arr[i]) {
+                answer.push(arr[i]);
+            }
         }
     }
+    
     
     return answer;
 }
